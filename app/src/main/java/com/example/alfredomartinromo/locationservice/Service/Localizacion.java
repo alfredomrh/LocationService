a6b1bcb672662f1047a8e38c7eafa7a02ef95eb1
Alfredo Martín - Romo Holguín <alfredomrh@hotmail.com>
2017-02-23 19:38:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alfredomartinromo/locationservice/Service/Localizacion.java
@@ -0,0 +1,61 @@
+package com.example.alfredomartinromo.locationservice.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.IBinder;
+
+//mirar este video aunque no utiliza el servicio correctamente
+// https://www.youtube.com/watch?v=DsBTQ4F6n7s
+
+/**este servicio implementa no va a ser bind e implementa la interfaz de localización
+ *
+ */
+
+public class Localizacion extends Service implements LocationListener {
+    public Localizacion() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
